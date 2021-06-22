ec9187f1ba87bcbf1e26c94e283b930b2def15b3
Leonardo Souza <leogsouza@gmail.com>
2021-06-21 23:00:34 -0300

    Add tests for store

--- internal/log/store.go
+++ internal/log/store.go
@@ -36,6 +36,7 @@ func newStore(f *os.File) (*store, error) {
 	}, nil
 }
 
+// Append persists the given bytes to the store
 func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -53,6 +54,7 @@ func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
 	return uint64(w), pos, nil
 }
 
+// Read returns the record stored at the given position
 func (s *store) Read(pos uint64) ([]byte, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -71,6 +73,7 @@ func (s *store) Read(pos uint64) ([]byte, error) {
 	return b, nil
 }
 
+// ReadAt reads the length of bytes into p beginning at the off offset in the store file
 func (s *store) ReadAt(p []byte, off int64) (int, error) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -80,6 +83,7 @@ func (s *store) ReadAt(p []byte, off int64) (int, error) {
 	return s.File.ReadAt(p, off)
 }
 
+// Close persits any buffered data before closing the file
 func (s *store) Close() error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
